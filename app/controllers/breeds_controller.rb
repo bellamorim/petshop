bf9b3ec402d542a1bbf49d66fe53a2499a027ad4
bellamorim <isabel.amorim17@gmail.com>
2019-07-13 14:26:45 -0300

    adiciona um novo campo detalhe na entidade raça e melhora o design da tela de mostrar raca

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -57,7 +57,7 @@ class BreedsController < ApplicationController
     @breed.destroy
     respond_to do |format|
       format.html { redirect_to breeds_url, notice: 'Breed was successfully destroyed.' }
-      format.json { head :no_content }
+      format.json { head :no_content }  
     end
   end
 
@@ -69,6 +69,6 @@ class BreedsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def breed_params
-      params.require(:breed).permit(:nome)
+      params.require(:breed).permit(:nome, :detail)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_140428) do
+ActiveRecord::Schema.define(version: 2019_07_13_144347) do
 
   create_table "breeds", force: :cascade do |t|
     t.string "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "detail"
   end
 
   create_table "especies", force: :cascade do |t|
